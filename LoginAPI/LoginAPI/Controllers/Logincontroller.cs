01d15c7937434322eb8a367e17fabfa5dcf2ceb2
your username <sihlembhem@gmail.com>
2019-11-27 15:37:31 +0200

    LoginAPI created

---
+++ LoginAPI/LoginAPI/Controllers/Logincontroller.cs
@@ -0,0 +1,17 @@
+﻿[Route("Api/Login/createcontact")]
+  [HttpPost]
+  public object createcontact(Registration Lvm)
+{
+    try
+    {
+        DemoLoginEntities db = new DemoLoginEntities();
+        Employeemaster Em = new Employeemaster();
+
+        if(Em.UserId==0)
+
+
+
+    }
+}
+
+}
---
+++ LoginAPI/dbo.Table.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Employeemaster]
+(
+  [UserId][int]IDENTITY( 1,1) NOT NULL,
+ [UserName][varchar](50) NOT NULL,
+ [LoginName][varchar](50) NULL,
+ [PassWORDrd][varchar](50) NOT NULL,
+ [Email][varchar](50) NULL,
+ [ContactNo][varchar](15) NULL,
+ [Address][varchar](50) NULL,
+ [IsApproved][int] NULL,
+ [Status][int] NULL,
+ [TotalCnt][int] NULL,
+
+ PRIMARY KEY CLUSTERED
+(
+ [UserId] ASC
+)
+WITH(PAD_INDEX =OFF, STATISTICS_NORECOMPUTE=OFF, IGNORE_DUP_KEY=OFF, ALLOW_ROW_LOCKS=ON, ALLOW_PAGE_LOCKS=ON) ON [PRIMARY]
+)
